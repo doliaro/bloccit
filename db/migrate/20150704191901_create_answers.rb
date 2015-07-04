9a2ec729dde56b070949dc842a04c9db9cff09c1
Dom Oliaro <dom@jackrabbitsystems.com>
2015-07-04 13:37:20 -0600

    migration

--- db/migrate/20150704191901_create_answers.rb
+++ db/migrate/20150704191901_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :body
-      t.reference :question
+      t.references :question
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150704165439) do
+ActiveRecord::Schema.define(version: 20150704191901) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "question_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -25,6 +32,14 @@ ActiveRecord::Schema.define(version: 20150704165439) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
