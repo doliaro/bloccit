982c503f82523014e8315cdb4ac7b97b77d66939
Dom Oliaro <dom@jackrabbitsystems.com>
2015-07-11 14:24:03 -0600

    advertising

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
 
 	resources :posts
 
+	resources :advertisements
+
   	get 'about' => 'welcome#about'
 
   	get 'contact' => 'welcome#contact'
+
+  	get 'posts' => 'welcome#posts'
 
  	root to: 'welcome#index'
 
