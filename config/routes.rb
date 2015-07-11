7661985bbc18c8716fd3cf1fce6dca5a71b78721
Dom Oliaro <dom@jackrabbitsystems.com>
2015-07-11 13:18:27 -0600

    testing with crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
+  get 'contact' => 'welcome#contact'
 
   root to: 'welcome#index'
 
