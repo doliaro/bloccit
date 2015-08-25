0a6d5a773b15313be386ffeaf10aee838614df13
Dom Oliaro <dom@jackrabbitsystems.com>
2015-08-25 14:29:56 -0600

    further authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,3 +49,49 @@
      end
    end
  end
+class PostsController < ApplicationController
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+    authorize @post
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post.user = current_user
+    authorize @post
+    if @post.save
+      flash[:notice] = "Post was saved"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] =  "Post was updated"
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again"
+      render :edit
+    end
+  end
+end
+
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,15 @@
  class PostPolicy < ApplicationPolicy
+    class Scope < Scope
+       def resolve
+        if user.admin? || user.moderator?
+            scope.all
+        elsif user.present?
+            scope.where(user_id: user.id) 
+        else
+            scope.none
+        end
+
+        end
     def index?
         true
     end
